9369771dade32f2222675d3eeb043ed8bdfae2b8
cnhung2001 <63276586+cnhung2001@users.noreply.github.com>
2022-03-11 12:37:02 +0700

    lesson 14

--- android/app/src/main/kotlin/com/example/hello_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hello_app/MainActivity.kt
@@ -1,6 +1,47 @@
-package com.example.hello_app
+package com.example.flutter_browser_app
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    protected fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(FlutterEngine(this))
+        MethodChannel(getFlutterEngine().getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
+                object : MethodCallHandler() {
+                    fun onMethodCall(call: MethodCall, result: result) {
+                        val url: String = call.argument("url")
+                        if (call.method.equals("openBrowser")) {
+                            openBrowser(call, result, url)
+                        } else {
+                            result.notImplemented()
+                        }
+                    }
+                }
+        )
+    }
+
+    private fun openBrowser(call: MethodCall, result: result, url: String) {
+        val activity: Activity = this
+        if (activity == null) {
+            result.error("ACTIVITY_NOT_AVAILABLE",
+                    "Browser cannot be opened without foreground activity", null)
+            return
+        }
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        activity.startActivity(intent)
+        result.success(true as Any)
+    }
+
+    companion object {
+        private const val CHANNEL = "flutterapp.tutorialspoint.com/browser"
+    }
 }
